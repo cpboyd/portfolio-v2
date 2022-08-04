9f91918400669d9ffa1ef9c0db8133ae27940da4
Chris Boyd <6323077+cpboyd@users.noreply.github.com>
2022-08-04 14:56:09 -0400

    bug: nested page export

--- www/src/templates/prose.tsx
+++ www/src/templates/prose.tsx
@@ -6,28 +6,7 @@ import { Heading } from "../components/typography/heading"
 import { Spacer } from "../components/blocks/spacer"
 import { SEO } from "../components/seo"
 import { article } from "../constants/json-ld"
-
-const ProseTemplate: React.FC<PageProps<WritingViewDataProps>> = ({
-  data: { post },
-  location: { pathname },
-  children: mdxContent,
-}) => (
-  <WritingView post={post} mdxContent={mdxContent} pathname={pathname} type="prose">
-    <Text
-      color="textEmphasized"
-      fontWeight={500}
-      textAlign="center"
-      fontSize={[`md`, null, null, `1.125rem`, `1.3125rem`]}
-    >
-      {post.category.name}
-    </Text>
-    <Spacer size={6} axis="vertical" />
-    <Heading as="h1" textAlign="center">
-      {post.title}
-    </Heading>
-    <Spacer size={[16, null, null, 20]} axis="vertical" />
-  </WritingView>
-)
+import ProseTemplate from "../pages/prose"
 
 export default ProseTemplate
 
